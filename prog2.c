7be6b9514b9ef55c5b5405d3f44097b712daebcc
Akihiro1488 <akihiro1488@gmail.com>
2015-06-02 22:42:34 +0700

    Update prog2.c

--- prog2.c
+++ prog2.c
@@ -1,22 +1,37 @@
-#include <iostream>
- #include <cstdlib>
- #include <cmath>
-
- using namespace std;
-
- float a,b,h;
-
- float calculateValue(float x){
-     return (1 / M_PI) * (1 + (1 - cos(2*x)) / (1 + cos(2*x)) );
- }
-
- int main(){
-     cout << "enter a "; cin >> a; cout << endl;
-     cout << "enter b "; cin >> b; cout << endl;
-     cout << "enter h "; cin >> h; cout << endl;
-     for(; a <= b; a += h){
-         cout << "X = " <<a << " Y = " << calculateValue(a)
-                 <<endl;
-     }
-     return 0;
- }
+#include <iostream> //подключение библиотеки ввода - вывода
+#include <cmath> //подключение мат. функций
+using namespace std; //открытие пространства стандартных имён библиотеки
+#define PI 3.14159265 //запись константы пи
+int main()
+{
+int nu;// nu - счётчик циклов
+float y, x, a, b, h, us;
+do
+{cout << "enter the value of the point A:";
+cin>> a;
+cout << "enter the value of the point B:";
+cin>> b;}
+while (b<a);
+cout << "enter the step value:";
+cin>> h;
+x=a;
+if (h<(b-a))
+{nu=1;
+ us=0;
+	while (x<=b)
+	{us=cos(x);
+		if (us=0)
+		 {cout << "function is not defined";
+		 x=x+h;
+		 nu=nu+1;}
+		else
+		 {y=((1/PI)*((1+tan(x))*(1+tan(x))));}
+		cout <<nu << "x=" << x << "\n";
+		cout <<nu << "y=" << y << "\n";
+		cout <<"______________________\n";
+		x=x+h;
+		nu=nu+1;}
+}
+	system ("pause");
+	return (0);
+}
